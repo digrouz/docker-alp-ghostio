6f43fd50628c679c39f6e4a2d2a531ff167f0e16
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2016-10-25 13:49:06 +0000

    added docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+local MYUSER="ghost"
+local MYGID="10010"
+local MYUID="10010"
+
+ConfigureUser () {
+  # Managing user
+  if [ -n "${DOCKUID}" ]; then
+    MYUID="${DOCKUID}"
+  fi
+  # Managing group
+  if [ -n "${DOCKGID}" ]; then
+    MYGID="${DOCKGID}"
+  fi
+  local OLDHOME
+  local OLDGID
+  local OLDUID
+  bin/grep -q "${MYUSER}" /etc/passwd
+  if [ $? -eq 0 ]; then
+    OLDUID=$(/usr/bin/id -u "${MYUSER}")
+    OLDGID=$(/usr/bin/id -g "${MYUSER}")
+    if [ "${DOCKUID}" != "${OLDUID}" ]; then
+      OLDHOME=$(/bin/echo "~${MYUSER}")
+      /usr/sbin/deluser "${MYUSER}"
+      /usr/bin/logger "Deleted user ${MYUSER}"
+    fi
+    /bin/grep -q "${MYUSER}" /etc/group
+    if [ $? -eq 0 ]; then
+      local OLDGID=$(/usr/bin/id -g "${MYUSER}")
+      if [ "${DOCKGID}" != "${OLDGID}" ]; then
+        /usr/sbin/delgroup "${MYUSER}"
+        /usr/bin/logger "Deleted group ${MYUSER}"
+      fi
+    fi
+  fi
+  /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
+  /usr/sbin/adduser -S -D -H -s /sbin/nologin -G "${MYUSER}" -h "${OLDHOME}" -u "${MYUID}" "${MYUSER}"
+  if [ -n "${OLDUID}" ] && [ "${DOCKUID}" != "${OLDUID}" ]; then
+    /usr/bin/find / -user "${OLDUID}" -exec /bin/chown ${MYUSER} {} \;
+  fi
+  if [ -n "${OLDGID}" ] && [ "${DOCKGID}" != "${OLDGID}" ]; then
+    /usr/bin/find / -group "${OLDGID}" -exec /bin/chgrp ${MYUSER} {} \;
+  fi
+}
+
+ConfigureUser
+
+if [ "$1" == 'ghost' ]; then
+    if [ ! -d /ghost ]; then
+      mkdir /ghost
+    fi
+    if [ -d /ghost ]; then
+      chown -R "${MYUSER}":"${MYUSER}" /ghost
+      chmod -R 0750 /ghost
+    fi
+    cd /ghost
+    exec /sbin/su-exec "${MYUSER}" npm start --production
+fi
+
+exec "$@"
